1f6aaebc5c131bdedcdcce563f862440ab351eef
Amy MacKinnon <amy@amy-mac.com>
2013-10-20 15:27:59 -0700

    Attempted to create tests

--- spec/models/beach_spec.rb
+++ spec/models/beach_spec.rb
@@ -1,5 +1,7 @@
 require 'spec_helper'
 
 describe Beach do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:main_image) }
 end
