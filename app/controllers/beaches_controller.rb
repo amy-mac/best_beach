82b5ac82ecdaac42e9bdfe7b31d81ff7a1d3fd82
Amy MacKinnon <amy@amy-mac.com>
2013-10-19 18:38:13 -0700

    Removed yahoo_weatherman gem, added yahoo_weather gem

--- app/controllers/beaches_controller.rb
+++ app/controllers/beaches_controller.rb
@@ -3,12 +3,18 @@ class BeachesController < ApplicationController
   def index
     client = Yelp::Client.new
  
-    request = Yelp::V1::Review::Request::Location.new(
-                :address => '650 Mission St',
-                :city => 'San Francisco',
-                :state => 'CA',
-                :radius => 2,
-                :term => 'cream puffs')
+    request = Yelp::V2::Search::Request::GeoPoint.new(
+              :latitude => 37.788022,
+              :longitude => -122.399797,
+              :category_filter => ['beaches'],
+              :radius_filter => 40000,
+              :sort => 2)
     @response = client.search(request)['businesses']
+
   end
+  
+  def show
+    @beach = Beach.find_by_yelp_id(params[:id])
+  end
+
 end
--- app/helpers/beaches_helper.rb
+++ app/helpers/beaches_helper.rb
@@ -1,2 +1,8 @@
 module BeachesHelper
+  
+  def weather(postal_code)
+    client = YahooWeather::Client.new
+    response = client.fetch_by_location("#{postal_code}")
+    return "#{response.condition.temp} #{response.units.temperature}"
+  end
 end
