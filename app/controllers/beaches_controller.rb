296b5c30e50fff4a9f2bdcce8c91d56e5de950c6
Amy MacKinnon <amy@amy-mac.com>
2013-10-23 16:27:38 -0700

    Changed coords constant to a cookie

--- app/controllers/beaches_controller.rb
+++ app/controllers/beaches_controller.rb
@@ -1,5 +1,4 @@
 class BeachesController < ApplicationController
-  COORDS = []
   
   def index
 
@@ -8,14 +7,13 @@ class BeachesController < ApplicationController
   def create
     @latitude = params[:latitude]
     @longitude = params[:longitude]
-    
-    COORDS << @latitude
-    COORDS << @longitude
+
+    cookies[:coordinates] = [@latitude, @longitude]
     
     y = Information.new
     @response = y.get_beach_list(@latitude, @longitude)
     
-    @origin = @latitude + "," + @longitude
+    @origin = origin_set
 
   end
   
@@ -25,23 +23,23 @@ class BeachesController < ApplicationController
     y = Information.new
     @response = y.get_beach(@beach.yelp_id)
     
-    # @origin = COORDS[0] + "," + COORDS[1]
-    @origin = "37.7798776,-122.3949111"
-    @destination = @response['location']['address'][0] + "," + @response['location']['postal_code']
+    @origin = origin_set
+    @destination = []
+    @destination << @response['location']['address'][0].split(" ")
+    @destination << @response['location']['postal_code']
+    @destination = @destination.flatten.join(",")
 
     @drive_time = Typhoeus.get(
       "http://www.mapquestapi.com/directions/v2/route?key=Fmjtd%7Cluubnu0all%2C7n%3Do5-9u1s0z&from=#{@origin}&to=#{@destination}"
       )
-
     @results = JSON.parse(@drive_time.body)
+
 
     @directions = "https://maps.google.com/maps?saddr=#{@origin}&daddr=#{@destination}"
 
     w = Wunderground.new('5d5c82de5f22fc4e')
     @weather = w.conditions_and_astronomy_and_hourly_for(@response['location']['postal_code'])
     
-    # client = Wikipedia::Client.new
-    # @wiki_page = client.find( @beach.name )
   end
 
 end
--- app/helpers/beaches_helper.rb
+++ app/helpers/beaches_helper.rb
@@ -1,5 +1,15 @@
 module BeachesHelper
-  
+
+  def origin_set
+    if cookies[:coordinates].is_a? Array
+      origin = cookies[:coordinates][0] + "," + cookies[:coordinates][1]
+    else
+      origin = cookies[:coordinates].split("&")
+      origin = origin[0] + "," + origin[1]
+    end
+    return origin
+  end
+
   def weather(postal_code)
     w = Wunderground.new('5d5c82de5f22fc4e')
     @weather = w.conditions_and_astronomy_and_hourly_for(postal_code)
