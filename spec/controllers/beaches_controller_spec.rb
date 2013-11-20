12f6b29f07851ffba3100cf4dbbf223232e5e0c1
Amy MacKinnon <amy@amy-mac.com>
2013-11-20 15:39:24 -0800

    Tests

--- spec/controllers/beaches_controller_spec.rb
+++ spec/controllers/beaches_controller_spec.rb
@@ -13,20 +13,12 @@ describe BeachesController do
     before :each do
       @params = {latitude: 37.7799177, longitude: -122.39494229999998}
     end
-    # it 'should assign @latitude' do
-    #   @latitude = 37.7799177
-    #   @latitude.should_not == nil
-    # end
 
     it 'should create a cookie' do
       post(:create, @params)
       response.cookies['coordinates'].should_not == nil
     end
 
-    # it 'should create a new instance of the information class' do
-    #   test = Information.new
-    #   test.should be_an_instance_of(Information)
-    # end
   end
 
   describe '#show' do
