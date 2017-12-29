07716b22d0f0dba50bfe8819d2e9f5c18b36b04e
Amy MacKinnon <amy@amy-mac.com>
2017-12-28 17:51:24 -0800

    Delete secret_token.rb

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-BestBeach::Application.config.secret_token = 'c3472d151a82bb91a1a87472c78bf7009b8baac75fe0ee0b46b0d3daf325dee8522eb589fcd3575eca7caf3ee5739bbd3fee1063d985c6e5378fe3633b55ac29'
